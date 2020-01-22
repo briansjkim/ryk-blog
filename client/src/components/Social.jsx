122876cdfa354f81fbd9b5def4394c02bb05a68b
Brian Kim <briaansjkim@gmail.com>
2020-01-22 14:11:54 -0500

    added social media links and added extra css

--- client/src/components/Social.jsx
+++ client/src/components/Social.jsx
@@ -4,11 +4,11 @@ import styles from './css/index.css';
 const Social = () => {
   return (
     <div className={styles.social_section}>
-      <p>"My name is Rachel"</p>
+      <p>"I am overflowing with words i do not have"</p>
       <div>
         <a className={styles.social_link} href="https://twitter.com/racheelkangg/" target="_blank"><i className="fab fa-twitter fa-1x"></i></a>
         <a className={styles.social_link} href="https://www.instagram.com/rchlkang/" target="_blank"><i className="fab fa-instagram fa-1x"></i></a>
-        <a className={styles.social_link} href="https://www.youtube.com/user/raachelkangg/"><i className="fab fa-youtube fa-1x"></i></a>
+        <a className={styles.social_link} target="_blank" href="https://www.youtube.com/user/raachelkangg/"><i className="fab fa-youtube fa-1x"></i></a>
       </div>
     </div>
   )
