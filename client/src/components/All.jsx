d9ebee5b0aff6e88f460b2e031b0f1d87a78a7ab
Brian Kim <briaansjkim@gmail.com>
2020-01-23 15:10:05 -0500

    made some temporary placements for future components. edited css

--- client/src/components/All.jsx
+++ client/src/components/All.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Nav from './Nav.jsx';
+import Social from './Social.jsx';
+import Header from './Header.jsx';
 import styles from './css/index.css';
 
 const All = () => {
   return (
     <div className={styles.all}>
       <Nav />
-      <h1>All Blogs</h1>
+      <Header />
+      <div className={styles.all_mid}>
+        {/* Blogs  */}
+        {/* <div className={styles.all_social}>
+          <Social />
+        </div> */}
+      </div>
     </div>
   )
 }
