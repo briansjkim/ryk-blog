3c1c613a73967e429a434f45bf94cf76cb2db847
Brian Kim <briaansjkim@gmail.com>
2020-01-23 15:01:03 -0500

    added text and edited css

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -4,9 +4,10 @@ import styles from './css/index.css';
 
 const Image = () => {
   return (
-      <div className={styles.image_section}>
-        <img className={styles.image} src="https://ryk.s3.us-east-2.amazonaws.com/img_4273.jpg"></img>
-      </div>
+    <div className={styles.image_section}>
+      <img className={styles.image} src="https://ryk.s3.us-east-2.amazonaws.com/img_4273.jpg"></img>
+      <p>create an outlet in order to solidify ideas into reality. memories into pictures. revelations into testimonies. and life into the <br></br>way you want it to look!</p>
+    </div>
   )
 }
 
