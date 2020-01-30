4803e0aff28c217ebbd94e592d1e72e9ac042558
Brian Kim <briaansjkim@gmail.com>
2020-01-30 16:46:48 -0500

    removed unwanted content and edited css

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -6,7 +6,6 @@ const Image = () => {
   return (
     <div className={styles.image_section}>
       <img className={styles.image} src="https://ryk.s3.us-east-2.amazonaws.com/img_4273.jpg"></img>
-      <p>create an outlet in order to solidify ideas into reality. memories into pictures. revelations into testimonies. and life into the <br></br>way you want it to look!</p>
     </div>
   )
 }
