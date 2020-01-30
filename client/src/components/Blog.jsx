2c89f6e12cf6a091c60fadede88236bbf593809c
Brian Kim <briaansjkim@gmail.com>
2020-01-30 16:29:48 -0500

    added styling to each blog post and placed repeated styling in parent element

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -5,8 +5,10 @@ const Blog = ({ blog }) => {
   return (
     <div className={styles.blog_section}>
       <h3 className={styles.blog_date}>{blog.date}</h3>
-      <h1>{blog.title}</h1>
-      <p>{blog.message}</p>
+      <h1 className={styles.blog_title}>{blog.title}</h1>
+      <div className={styles.blog_message_section}>
+        <p className={styles.blog_message}>{blog.message}</p>
+      </div>
     </div>
   )
 }
