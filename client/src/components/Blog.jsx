94137f140d735c7748a01c7c75d224195586459c
Brian Kim <briaansjkim@gmail.com>
2020-01-30 15:21:14 -0500

    rendered first blog

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -3,8 +3,12 @@ import styles from './css/index.css';
 
 const Blog = ({ blog }) => {
   return (
-    <div>
-
+    <div className={styles.blog_section}>
+      <h3 className={styles.blog_date}>{blog.date}</h3>
+      <h1>{blog.title}</h1>
+      <p>{blog.message}</p>
     </div>
   )
 }
+
+export default Blog;
--- client/src/components/Blogs.jsx
+++ client/src/components/Blogs.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Blog from './Blog.jsx';
 import styles from './css/index.css';
 
 const Blogs = ({ blogs }) => {
   return (
     <div className={styles.blogs}>
-      <h1>Blogs</h1>
       <div className={styles.blogs_section}>
-
+        {blogs.map((blog, index) =>
+          <Blog blog={blog} key={index} />
+        )}
       </div>
     </div>
   )
