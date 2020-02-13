a5213673dd4daca158cdf639067a444cbd9dafb6
Brian Kim <briaansjkim@gmail.com>
2020-02-13 17:59:41 -0500

    edited css file

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -9,6 +9,7 @@ const Blog = ({ blog }) => {
       <div className="blog_message_section">
         <p className="blog_message">{blog.message}</p>
       </div>
+      <p className="open_blog">Read More...</p>
     </div>
   )
 }
