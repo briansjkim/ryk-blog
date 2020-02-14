7704fdb1326fbddf249cb9b4ae02cf66dfb27f9a
Brian Kim <briaansjkim@gmail.com>
2020-02-14 16:43:20 -0500

    added link component in order to go back to the home page

--- client/src/components/FullBlog.jsx
+++ client/src/components/FullBlog.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import Nav from './Nav.jsx';
+import { Link } from 'react-router-dom';
 import Social from './Social.jsx';
 
 const FullBlog = (props) => {
-  console.log(props);
   return (
-    <div className="full-blog">
-      <h3>{props.location.state.blog.date}</h3>
-      <h1>{props.location.state.blog.title}</h1>
+    <div>
+      <Nav />
+      <Link to='/'><i className="far fa-caret-square-left fa-2x"></i></Link>
       <div>
-        <p>{props.location.state.blog.message}</p>
+        <h3>{props.location.state.blog.date}</h3>
+        <h1>{props.location.state.blog.title}</h1>
+        <div>
+          <p>{props.location.state.blog.message}</p>
+        </div>
+        <div>
+          <Social />
+        </div>
       </div>
-      <Social />
     </div>
   )
 }
