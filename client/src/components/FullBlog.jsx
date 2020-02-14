18d9469b612eff0ab28bc4edecd6ef80400be186
Brian Kim <briaansjkim@gmail.com>
2020-02-14 16:52:37 -0500

    adding css full blog page

--- client/src/components/FullBlog.jsx
+++ client/src/components/FullBlog.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import Nav from './Nav.jsx';
 import { Link } from 'react-router-dom';
 import Social from './Social.jsx';
+import './scss/index.scss';
 
 const FullBlog = (props) => {
   return (
-    <div>
+    <div className="full-blog">
       <Nav />
-      <Link to='/'><i className="far fa-caret-square-left fa-2x"></i></Link>
-      <div>
-        <h3>{props.location.state.blog.date}</h3>
-        <h1>{props.location.state.blog.title}</h1>
-        <div>
-          <p>{props.location.state.blog.message}</p>
+      <div className="full-blog-mid">
+        <div className="full-blog-details">
+          <h3>{props.location.state.blog.date}</h3>
+          <h1>{props.location.state.blog.title}</h1>
+          <div>
+            <p>{props.location.state.blog.message}</p>
+          </div>
         </div>
-        <div>
+        <div className="full-blog-social">
           <Social />
         </div>
       </div>
