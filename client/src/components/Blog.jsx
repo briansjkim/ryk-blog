46b1a57899c867727b31c22713470de13a04ea02
Brian Kim <briaansjkim@gmail.com>
2020-02-17 15:59:15 -0500

    added another link component and used it with the title

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -6,7 +6,12 @@ const Blog = ({ blog }) => {
   return (
     <div className="blog_section">
       <h3 className="blog_date">{blog.date}</h3>
-      <h1 className="blog_title">{blog.title}</h1>
+      <Link to={{
+        pathname: '/blog',
+        state: { blog: blog }
+      }}>
+        <h1 className="blog_title">{blog.title}</h1>
+      </Link>
       <div className="blog_message_section">
         <p className="blog_message">{blog.message}</p>
       </div>
