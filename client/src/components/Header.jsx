09d52e103f004e6558be3fb1808b8bb8e76b648f
Brian Kim <briaansjkim@gmail.com>
2020-03-25 15:40:32 -0400

    deleted comment

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,6 @@ import All from './All.jsx';
 import { Link } from 'react-router-dom';
 import './scss/index.scss';
 
-// turn header into class component and pass all the blogs to link
 const Header = ({ blogs }) => {
   return (
     <div className="header">
