c659061d857f7a097d2f67e1290009e3e7750230
Brian Kim <briaansjkim@gmail.com>
2020-03-25 16:07:41 -0400

    switched component from functional to class

--- client/src/components/CreateBlog.jsx
+++ client/src/components/CreateBlog.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './scss/index.scss';
 
-const CreateBlog = () => {
-  return (
-    <div className="create-blog">
-      <p>Hello</p>
-    </div>
-  )
+class CreateBlog extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+      date: '',
+      message: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="create-blog">
+        <h1>Hello</h1>
+      </div>
+    )
+  }
 }
 
 export default CreateBlog;
