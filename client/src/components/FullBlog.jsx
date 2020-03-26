ba43734cd0b5b8e038eedb36994970be3dbcdff5
Brian Kim <briaansjkim@gmail.com>
2020-03-26 19:01:41 -0400

    changed FullBlog component from stateless to class in order to use goBack method

--- client/src/components/FullBlog.jsx
+++ client/src/components/FullBlog.jsx
@@ -5,24 +5,38 @@ import Header from './Header.jsx';
 import { withRouter } from 'react-router-dom';
 import './scss/index.scss';
 
-const FullBlog = (props) => {
-  return (
-    <div className="full-blog">
-      <Nav />
-      <div className="full-blog-mid">
-        <div className="full-blog-details">
-          <h3>{props.location.state.blog.date}</h3>
-          <h1>{props.location.state.blog.title}</h1>
-          <div>
-            <p>{props.location.state.blog.message}</p>
+class FullBlog extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.goBack = this.goBack.bind(this);
+  }
+
+  goBack() {
+    this.props.history.goBack();
+  }
+
+  render() {
+    return (
+      <div className="full-blog" >
+        <Nav />
+        <button onClick={this.goBack}>Go Back</button>
+        <div className="full-blog-mid">
+          <div className="full-blog-details">
+            <h3>{this.props.location.state.blog.date}</h3>
+            <h1>{this.props.location.state.blog.title}</h1>
+            <div>
+              <p>{this.props.location.state.blog.message}</p>
+            </div>
+          </div>
+          <div className="full-blog-social">
+            <Social />
           </div>
         </div>
-        <div className="full-blog-social">
-          <Social />
-        </div>
-      </div>
-    </div>
-  )
+      </div >
+
+    )
+  }
 }
 
 export default withRouter(FullBlog);
