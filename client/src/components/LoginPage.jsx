20250458de8c05208fe88de322d112f5c9494678
Brian Kim <briaansjkim@gmail.com>
2020-03-31 18:36:17 -0400

    init setup for LoginPage

---
+++ client/src/components/LoginPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import './scss/index.scss';
+
+class LoginPage extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default LoginPage;
